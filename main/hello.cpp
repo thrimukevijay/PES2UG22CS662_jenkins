30f1e35611f73f359d42777f72ebb3d51bfac41f
Lucky <thrimukevijay@gmail.com>
2025-03-11 19:34:15 +0530

    Updated hello.cpp to test Jenkins webhook trigger

--- main/hello.cpp
+++ main/hello.cpp
@@ -6,6 +6,6 @@ int main(void) {
   cout << "Hello, World\n";
   cout << "Hello, Jenkins\n";
   cout << "I have successfully built and run this  \n";
-  cout <<"task4"
+  cout <<"task4";
   return 0;
 }
